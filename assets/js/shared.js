c3380e0074edcd6d473eb9a2dead6c142d21da3a
India Evans <indie.evans@proton.me>
2025-01-14 10:37:19 -0500

    cleanup

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -1,9 +1,7 @@
-const nav = document.querySelector('nav');
 const navLinks = document.querySelectorAll('nav > .menu-item');
 const main = document.querySelector('main');
-let navVisible = false;
-displayNav();
-setTimeout(() => changeOpacity(main, 1), 750);
+displayNavLinks();
+setTimeout(displayMain, 750);
 
 function changeOpacity(element, amount) {
   element.style.opacity = amount;
@@ -11,25 +9,13 @@ function changeOpacity(element, amount) {
 function scrollToTop() {
   window.scrollTo(0, 0);
 }
-// TODO refactor
-// TODO only fade in nav when navigating from landing page
-function displayNav() {
-  if (!navVisible) {
-    // setTimeout(() => {
-    //   nav.classList.add('nav-background');
-    // }, 250);
-
-    navLinks.forEach((element, index) => {
-      setTimeout(() => {
-        changeOpacity(element, 1);
-      }, 250 * index);
-    });
-    navVisible = true;
-  } else {
-    // nav.classList.remove('nav-background');
-    navLinks.forEach(element => {
-      changeOpacity(element, 0);
-    });
-    navVisible = false;
-  }
+function displayNavLinks() {
+  navLinks.forEach((element, index) => {
+    setTimeout(() => {
+      changeOpacity(element, 1);
+    }, 250 * index);
+  });
+}
+function displayMain() {
+  changeOpacity(main, 1);
 }
